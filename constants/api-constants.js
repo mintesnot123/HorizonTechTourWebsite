72ac3c9d1e8b17137bed29bd4a5347c2d595f134
Mintesnot Yismaw <yismawmintesnot@gmail.com>
2021-11-30 05:05:18 -0800

    edited base url

--- constants/api-constants.js
+++ constants/api-constants.js
@@ -1,5 +1,6 @@
 const apiConstants = {
-    BASE_URL: `http://localhost:${process.env.PORT || 3000}/`,
+    /* BASE_URL: `http://localhost:${process.env.PORT || 3000}/`, */
+    BASE_URL: `https://chuditourandtravel.herokuapp.com/`,
 };
 
 module.exports = apiConstants;
